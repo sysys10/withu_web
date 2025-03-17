e5f6a8b9b34fea8afe5fe2ff702335abf60d9cc3
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-03-17 16:48:17 +0900

    Update route.ts

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,4 +1,5 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { NextResponse } from 'next/server'
 
 const Bucket = process.env.AMPLIFY_BUCKET
 // S3 클라이언트 설정
@@ -10,7 +11,7 @@ const s3 = new S3Client({
   }
 })
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     // 폼 데이터 처리
     const formData = await req.formData()
@@ -18,10 +19,10 @@ export async function POST(req: Request, res: Response) {
 
     // 업로드할 이미지 파일 수 제한
     if (files.length > 3) {
-      return new Response(
-        JSON.stringify({
+      return NextResponse.json(
+        {
           message: '업로드할 수 있는 이미지 파일의 수는 최대 3장입니다.'
-        }),
+        },
         { status: 400 }
       )
     }
@@ -41,23 +42,19 @@ export async function POST(req: Request, res: Response) {
         })
       )
 
-      return [`https://${Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${Key}`]
+      return `https://${Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${Key}`
     })
 
     const imgUrls = await Promise.all(uploadPromises)
 
     if (imgUrls.length === 1) {
-      return new Response(JSON.stringify({ data: imgUrls[0], message: 'OK' }), {
-        status: 200
-      })
+      return NextResponse.json({ data: imgUrls[0], message: 'OK' }, { status: 200 })
     }
 
     // 파일 개수가 여러개면 URL 배열 반환
-    return new Response(JSON.stringify({ data: [...imgUrls], message: 'OK' }), {
-      status: 200
-    })
+    return NextResponse.json({ data: imgUrls, message: 'OK' }, { status: 200 })
   } catch (error) {
     console.error('Error uploading files:', error)
-    return new Response(JSON.stringify({ message: '파일 업로드 중 오류가 발생했습니다.' }), { status: 500 })
+    return NextResponse.json({ message: '파일 업로드 중 오류가 발생했습니다.' }, { status: 500 })
   }
 }
