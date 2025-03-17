5a445781cb73b0879b56c49a8646060ef04dc77a
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-03-17 16:55:03 +0900

    Update page.tsx

--- src/app/add/course/page.tsx
+++ src/app/add/course/page.tsx
@@ -1,3 +1,11 @@
+import { handleCreateCourse } from '@/utils/handleMobileAction'
+
 export default function AddCoursePage() {
-  return <div>AddCoursePage</div>
+  handleCreateCourse()
+  return (
+    <div className='h-screen w-screen pt-40'>
+      AddCoursePage
+      <button onClick={handleCreateCourse}>Create Course</button>
+    </div>
+  )
 }
