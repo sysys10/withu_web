2d1754f163cad6224bc1a63a719aaf466e1683cb
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-03-17 17:38:14 +0900

    Update AddCourseButton.tsx

--- src/components/AddCourseButton.tsx
+++ src/components/AddCourseButton.tsx
@@ -3,5 +3,11 @@
 import { handleCreateCourse } from '@/utils/handleMobileAction'
 
 export default function AddCourseButton({ text }: { text: string }) {
-  return <button onClick={handleCreateCourse}>{text}</button>
+  return (
+    <button
+      className='bg-blue-400 rounded-lg text-white p-2'
+      onClick={handleCreateCourse}>
+      {text}
+    </button>
+  )
 }
